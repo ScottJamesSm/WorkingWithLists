ae0212f6c6e6106985bffdba725d66d8f1959da0
Scott James Smith <slocknog1982@gmail.com>
2018-06-11 15:22:27 -0400

    Worked with manuplating Lists

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,59 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
+    private List<String> nameList;
+    private Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Main mainClass = new Main();
+        mainClass.testingLists();
+    }
+
+    private void testingLists() {
+        nameList = new ArrayList<String>();
+
+        nameList.add("Crystal");
+        nameList.add("Chris");
+        nameList.add("Bin");
+        nameList.add("Luke");
+// For Each Loop to see all items in arrayList.
+        for (String name : nameList) {
+            System.out.println(name);
+        }
+
+        System.out.println("See a single item in the array list:");
+        System.out.println(nameList.get(2));
+
+        System.out.println("Remove a single item and list what is left in the ArrayList: ");
+        nameList.remove(0);
+
+        for (String name : nameList) {
+            System.out.println(name);
+
+        }
+
+
+        System.out.println("viewing array List using Iteration loop with numbers");
+        for (int i = 0; i < nameList.size(); i++) {
+            System.out.println(i + nameList.get(i));
+        }
+        System.out.println("which number would you like to remove?");
+        int numberToRemove = input.nextInt() - 1;
+        if(numberToRemove > nameList.size()) {
+            System.out.println("that is not a valid number");
+        } else {
+
+            nameList.remove(numberToRemove);
+        }
+
+        System.out.println("list after take out selected number");
+
     }
 }
+
